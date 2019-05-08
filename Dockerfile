321ebdcf759e631b466f1a1612c9d091bd68e55d
myyong <myong@turing.ac.uk>
2019-05-08 11:33:38 +0100

    Open server port

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
 FROM kartoza/postgis:10.0-2.4
-
 RUN apt-get update;
 RUN apt-get install -y nano
+
+EXPOSE 5432
--- scripts/createTables.sh
+++
@@ -1,8 +0,0 @@
-create table traffic (
-vehicleID serial NOT NULL,
-vehicleCategory integer,
-lat float,
-lon float,
-x float,
-y float,
-tInSeconds float);
